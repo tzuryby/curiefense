4a4a184d59bf82bfdc9b1eb43850cc7129c2732b
Xavier <xavier@reblaze.com>
2022-05-11 07:42:03 +0200

    Remove unused confserver config & scripts post-#826
    Signed-off-by: Xavier <xavier@reblaze.com>

--- curiefense/images/confserver/init/start_nginx.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-# This script is to avoid the test failure caused by the unsynchronized update of the curiefense-helm repository.
-
-while true;do
-  echo "Discarded..."
-  sleep 60
-done
