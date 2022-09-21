ed5da69dff09f6971897628082e4d08935b78a1b
Simon Marechal <bartavelle@gmail.com>
2022-09-21 09:12:46 +0200

    Make nginx error log configurable
    Signed-off-by: Simon Marechal <bartavelle@gmail.com>

--- curiefense/images/curieproxy-nginx/start.sh
+++ curiefense/images/curieproxy-nginx/start.sh
@@ -2,8 +2,9 @@
 FILEBEAT="${FILEBEAT:-yes}"
 AGGREGATED_LOG_FILE="${AGGREGATED_LOG_FILE:-/dev/stdout}"
 ACCESS_LOG="${ACCESS_LOG:-/dev/stdout}"
+ERROR_LOG="${ERROR_LOG:-/logs/error.log}"
 # shellcheck disable=SC2016
-envsubst '${TARGET_ADDRESS_A},${TARGET_PORT_A},${TARGET_ADDRESS_B},${TARGET_PORT_B},${AGGREGATED_STATS_LOGFILE},${ACCESS_LOG}' < /etc/nginx/conf.d/default.template > /etc/nginx/conf.d/default.conf
+envsubst '${TARGET_ADDRESS_A},${TARGET_PORT_A},${TARGET_ADDRESS_B},${TARGET_PORT_B},${AGGREGATED_STATS_LOGFILE},${ACCESS_LOG},${ERROR_LOG}' < /etc/nginx/conf.d/default.template > /etc/nginx/conf.d/default.conf
 if [ "$FILEBEAT" = "yes" ]
 then
   /usr/local/openresty/bin/openresty -g "daemon off;" | grep -v '^.$' | /usr/bin/filebeat --path.config /etc
