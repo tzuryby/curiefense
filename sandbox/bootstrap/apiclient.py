3b253cb0409e33ce4940961221abfb3c7fd73c1c
Zuri Bar Yochay <tzury@reblaze.com>
2022-10-21 18:57:51 +0300

    e2e.py points to prod
    Signed-off-by: Zuri Bar Yochay <tzury@reblaze.com>

--- sandbox/bootstrap/apiclient.py
+++ sandbox/bootstrap/apiclient.py
@@ -21,15 +21,20 @@ host = "34.65.106.27"
 port = "30000"
 url = f"http://{host}:{port}"
 
+
 def update():
     for doc in documents:
-        print (f"pushing {doc}")
-        json=open(f"{doc}.json", "r+b").read()
+        print(f"pushing {doc}")
+        json = open(f"{doc}.json", "r+b").read()
         response = requests.delete(f"{url}/api/v3/configs/{branch}/d/{doc}/")
-        response = requests.post(f"{url}/api/v3/configs/{branch}/d/{doc}/",
-            data = json, headers = {"content-type": "application/json"})
+        response = requests.post(
+            f"{url}/api/v3/configs/{branch}/d/{doc}/",
+            data=json,
+            headers={"content-type": "application/json"},
+        )
         if response.status_code > 399:
-            pprint (response.json())
+            pprint(response.json())
+
 
 if __name__ == "__main__":
     update()
