0262a5164437b9e993de862d2e99106cd1ad8c15
Zuri Bar Yochay <tzury@reblaze.com>
2022-10-22 14:28:54 +0300

    new ui integrated
    Signed-off-by: Zuri Bar Yochay <tzury@reblaze.com>

--- curiefense/ui/jest.setup.ts
+++
@@ -1,7 +0,0 @@
-import '@testing-library/jest-dom'
-import Vue from 'vue'
-import VueAxios from 'vue-axios'
-import axios from 'axios'
-import {jest} from '@jest/globals'
-Vue.use(VueAxios, axios)
-global.URL.createObjectURL = jest.fn()
