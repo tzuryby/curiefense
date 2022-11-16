9fc8c3f599ad0a4853958118dfbfd5c96d410112
Zuri Bar Yochay <tzury@reblaze.com>
2022-11-17 00:35:32 +0200

    bootstrap nginx config files
    Signed-off-by: Zuri Bar Yochay <tzury@reblaze.com>

---
+++ curiefense/images/curieproxy-nginx/nginx-conf-reload.sh
@@ -0,0 +1,38 @@
+#!/usr/bin/env bash
+
+set -o errexit
+set -o nounset
+set -o pipefail
+if [[ "${TRACE-0}" == "1" ]]; then
+    set -o xtrace
+fi
+
+source_file="/cf-config/current/config/customconf.tar.gz"
+target_dir="/etc/nginx/conf.d"
+temp_dir="/tmp/current-conf"
+
+if [ -f "$source_file" ]; then
+  mkdir -p ${temp_dir}
+  rm -rf ${temp_dir}/*
+  cp -a ${target_dir}/. ${temp_dir}/
+  rm -rf ${target_dir}/*
+  echo "Extract $source_file into $target_dir"
+  tar xzf ${source_file} -C ${target_dir}
+  echo "Test Nginx with new configuration"
+  nginx -t
+  ## check exit code of previous command
+  retVal=$?
+  if [ $retVal -ne 0 ];
+  then
+    echo "Nginx failed, restore config files" >&2
+    cp -a  ${temp_dir}/. ${target_dir}/
+    cp ${target_dir}/lua/customcode.lua /lua/customcode.lua
+  else
+      echo reloading nginx
+      nginx -s reload
+      echo "Nginx reloaded with the new config"
+  fi
+else
+  echo "${source_file} does not exist. Exiting $0" >&2
+  exit
+fi
--- curiefense/images/curieproxy-nginx/start.sh
+++ curiefense/images/curieproxy-nginx/start.sh
@@ -3,10 +3,13 @@ export FILEBEAT="${FILEBEAT:-yes}"
 export AGGREGATED_STATS_LOG_FILE="${AGGREGATED_LOG_FILE:-/var/log/nginx.metrics.json.log}"
 export ACCESS_LOG="${NGINX_ACCESS_LOG:-/var/log/nginx.access.json.log}"
 export ERROR_LOG="${NGINX_ERROR_LOG:-/var/log/nginx.error.log}"
-export CF_LOG_LEVEL="${CF_LOG_LEVEL:-info}"
-export NGINX_LOG_LEVEL="${NGINX_LOG_LEVEL:-info}"
+export CF_LOG_LEVEL="${CF_LOG_LEVEL:-error}"
+export NGINX_LOG_LEVEL="${NGINX_LOG_LEVEL:-error}"
 # shellcheck disable=SC2016
 envsubst '${TARGET_ADDRESS_A},${TARGET_PORT_A},${TARGET_ADDRESS_B},${TARGET_PORT_B},${AGGREGATED_STATS_LOG_FILE},${ACCESS_LOG},${ERROR_LOG},${CF_LOG_LEVEL},${NGINX_LOG_LEVEL}' < /etc/nginx/conf.d/default.template > /etc/nginx/conf.d/default.conf
+
+/usr/local/bin/nginx-conf-watch.sh&
+
 if [ "$FILEBEAT" = "yes" ]
 then
   /usr/local/openresty/bin/openresty -g "daemon off;" | grep -v '^.$' | /usr/bin/filebeat --path.config /etc
