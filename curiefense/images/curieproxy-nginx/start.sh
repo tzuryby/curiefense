f0d3be90a3a46c135af235759ff5054f322a95c2
Xavier <xavier@reblaze.com>
2022-11-23 08:36:36 +0100

    Ignore shellcheck for quotes in envsubst calls
    Signed-off-by: Xavier <xavier@reblaze.com>

--- curiefense/images/curieproxy-nginx/start.sh
+++ curiefense/images/curieproxy-nginx/start.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
+
+# shellcheck disable=SC2016
 envsubst '${TARGET_ADDRESS_A},${TARGET_PORT_A},${TARGET_ADDRESS_B},${TARGET_PORT_B},${AGGREGATED_STATS_LOG_FILE},${NGINX_ACCESS_LOG},${NGINX_ERROR_LOG},${CF_LOG_LEVEL},${NGINX_LOG_LEVEL}' < /usr/local/openresty/nginx/conf/nginx.conf > /usr/local/openresty/nginx/conf/nginx.conf.1
 mv /usr/local/openresty/nginx/conf/nginx.conf.1 /usr/local/openresty/nginx/conf/nginx.conf
 
