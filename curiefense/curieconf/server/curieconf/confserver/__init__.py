tzuryby/curiefense

commit 7b580c5b063daa83c505abe71dfe79f8d1e919fd

Update __init__.py

curiefense/curieconf/server/curieconf/confserver/__init__.py
@@ -16,7 +16,7 @@ app = Flask(__name__)
 
 CORS(app, resources={r"/*": {"origins": "*"}})
 
-app.register_blueprint(api_v3.api_bp, url_prefix="/api/v3")
+app.register_blueprint(api_v3.api_bp, url_prefix=os.environ.get("SWAGGER_BASE_PATH", "/api/v3"))
 
 
 def drop_into_pdb(app, exception):

commit 2eeda8b821ffe2234774e943e237b48d312e8aca

Fix python formatting (regression in 7b580c5b06)
Signed-off-by: Xavier <xavier@reblaze.com>

curiefense/curieconf/server/curieconf/confserver/__init__.py
@@ -16,7 +16,9 @@ app = Flask(__name__)
 
 CORS(app, resources={r"/*": {"origins": "*"}})
 
-app.register_blueprint(api_v3.api_bp, url_prefix=os.environ.get("SWAGGER_BASE_PATH", "/api/v3"))
+app.register_blueprint(
+    api_v3.api_bp, url_prefix=os.environ.get("SWAGGER_BASE_PATH", "/api/v3")
+)
 
 
 def drop_into_pdb(app, exception):