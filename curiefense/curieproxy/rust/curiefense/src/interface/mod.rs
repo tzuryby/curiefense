6605d0da302775fc8f1758b3637e0ccb5af236c8
Arbel Axelrod <arbela@reblaze.com>
2023-04-17 14:04:21 +0300

    fixed format
    Signed-off-by: Arbel Axelrod <arbela@reblaze.com>

--- curiefense/curieproxy/rust/curiefense/src/interface/mod.rs
+++ curiefense/curieproxy/rust/curiefense/src/interface/mod.rs
@@ -235,7 +235,7 @@ pub fn jsonlog_rinfo(
     let mut ser = serde_json::Serializer::new(&mut outbuffer);
     let mut map_ser = ser.serialize_map(None)?;
     map_ser.serialize_entry("timestamp", now)?;
-     map_ser.serialize_entry(
+    map_ser.serialize_entry(
         "timestamp_min",
         &now.duration_trunc(chrono::Duration::minutes(1)).unwrap(),
     )?;
